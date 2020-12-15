8868f1f6d363d7909f8af24d47752bb1cba718a7
Valerie <akpotevalerie@gmail.com>
2020-12-16 00:46:41 +0100

    Add flipCard function and corresponding styling

---
+++ Memory Card Game/script.js
@@ -0,0 +1,10 @@
+const cards = document.querySelectorAll(".memory-card");
+
+function flipCard() {
+    this.classList.toggle("flip");
+}
+
+cards.forEach(card => {
+    card.addEventListener("click", flipCard);
+});
+
