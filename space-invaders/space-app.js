6b1e3fa9705503fb49b084b930f53e252d01114e
WillemDT369 <detroyerwillem@hotmail.com>
2020-12-15 10:24:03 +0100

    Make the aliens be able to move

--- space-invaders/space-app.js
+++ space-invaders/space-app.js
@@ -43,4 +43,28 @@ document.addEventListener("DOMContentLoaded", () => {
     // Keydown works for all keys, key press doesn't
     document.addEventListener("keydown", moveShooter);
 
+    // Move the alien invaders
+    function moveInvaders() {
+        const leftEdge = alienInvaders[0] % width === 0;
+        const rightEdge = alienInvaders[alienInvaders.length - 1] % width === width - 1;
+
+        if ((leftEdge && direction === -1) || (rightEdge && direction === 1)) {
+            direction = width;
+        } else if (direction === width) {
+            if (leftEdge) direction = 1;
+            else direction = -1;
+        }
+        for (let i = 0; i <= alienInvaders.length - 1; i++) {
+            squares[alienInvaders[i]].classList.remove("invader");
+        }
+        for (let i = 0; i <= alienInvaders.length - 1; i++) {
+            alienInvaders[i] += direction;
+        }
+        for (let i = 0; i <= alienInvaders.length - 1; i++) {
+            if (!alienInvadersTakenDown.includes(i)) {
+                squares[alienInvaders[i]].classList.add("invader");
+            }
+        }
+    }
+
 });
