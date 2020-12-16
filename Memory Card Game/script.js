cbf1cacab3f6ccfab4705258660aae2e34a947b3
Valerie <akpotevalerie@gmail.com>
2020-12-16 07:10:56 +0100

    Fix double-click-bug

--- Memory Card Game/script.js
+++ Memory Card Game/script.js
@@ -6,6 +6,7 @@ let firstCard, secondCard;
 
 function flipCard() {
     if (lockBoard) return;
+    if (this === firstCard) return;
 
     this.classList.add("flip");
 
@@ -15,7 +16,6 @@ function flipCard() {
         return;
     } 
 
-    hasFlippedCard = false;
     secondCard = this;
         
     checkforMatch();
@@ -29,6 +29,8 @@ function checkforMatch() {
 function disableCards() {
     firstCard.removeEventListener("click", flipCard);
     secondCard.removeEventListener("click", flipCard);
+
+    resetBoard();
 }
 
 function unflipCards() {
@@ -36,9 +38,16 @@ function unflipCards() {
     setTimeout(() => {
         firstCard.classList.remove("flip");
         secondCard.classList.remove("flip");
-        lockBoard = false;
+
+        resetBoard();
     }, 1500);
 }
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
 
 cards.forEach(card => {
     card.addEventListener("click", flipCard);
