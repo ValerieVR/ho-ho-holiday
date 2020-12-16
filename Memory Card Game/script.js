fd8f7f9abbcd54dd637cacb207963b0130bd3f55
Valerie <akpotevalerie@gmail.com>
2020-12-16 06:55:00 +0100

    Refecator code

--- Memory Card Game/script.js
+++ Memory Card Game/script.js
@@ -5,22 +5,34 @@ let firstCard, secondCard;
 
 function flipCard() {
     this.classList.add("flip");
+    
     if (!hasFlippedCard) {
         hasFlippedCard = true;
         firstCard = this;
-    } else {
-        hasFlippedCard = false;
-        secondCard = this;
-        if (firstCard.dataset.framework === secondCard.dataset.framework) {
-            firstCard.removeEventListener("click", flipCard);
-            secondCard.removeEventListener("click", flipCard);
-        } else {
-            setTimeout(() => {
-                firstCard.classList.remove("flip");
-                secondCard.classList.remove("flip");
-            }, 1500);
-        }
-    }
+        return;
+    } 
+
+    hasFlippedCard = false;
+    secondCard = this;
+        
+    checkforMatch();
+}
+
+function checkforMatch() {
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+    isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener("click", flipCard);
+    secondCard.removeEventListener("click", flipCard);
+}
+
+function unflipCards() {
+    setTimeout(() => {
+        firstCard.classList.remove("flip");
+        secondCard.classList.remove("flip");
+    }, 1500);
 }
 
 cards.forEach(card => {
