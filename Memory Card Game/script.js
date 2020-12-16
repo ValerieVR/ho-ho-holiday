fd812940352bcef624f5427ac211c01b2289c9c2
Valerie <akpotevalerie@gmail.com>
2020-12-16 01:41:29 +0100

    Define actions if cards match and when cards don't match

--- Memory Card Game/script.js
+++ Memory Card Game/script.js
@@ -11,8 +11,15 @@ function flipCard() {
     } else {
         hasFlippedCard = false;
         secondCard = this;
-        console.log(firstCard.dataset.framework);
-        console.log(secondCard.dataset.framework);
+        if (firstCard.dataset.framework === secondCard.dataset.framework) {
+            firstCard.removeEventListener("click", flipCard);
+            secondCard.removeEventListener("click", flipCard);
+        } else {
+            setTimeout(() => {
+                firstCard.classList.remove("flip");
+                secondCard.classList.remove("flip");
+            }, 1500);
+        }
     }
 }
 
