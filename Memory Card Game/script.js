efa080f2d6bb5eba4b38c6aeadffeeb459f28ba3
Valerie <akpotevalerie@gmail.com>
2020-12-16 01:16:42 +0100

    Change function to see if two cards are clicked

--- Memory Card Game/script.js
+++ Memory Card Game/script.js
@@ -1,7 +1,17 @@
 const cards = document.querySelectorAll(".memory-card");
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
-    this.classList.toggle("flip");
+    this.classList.add("flip");
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+    } else {
+        hasFlippedCard = false;
+        secondCard = this;
+    }
 }
 
 cards.forEach(card => {
