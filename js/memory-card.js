915d3f17a8826d0c211ec86a48f50bcb65ccef30
Valerie <akpotevalerie@gmail.com>
2020-12-18 01:10:49 +0100

    Add final button

--- js/memory-card.js
+++ js/memory-card.js
@@ -63,15 +63,16 @@ cards.forEach(card => {
     card.addEventListener("click", flipCard);
 });
 
+
 playMuteBTN.addEventListener("click", () => {
     if(count == 0) {
         count = 1;
         audio.play();
-        playMuteBTN.innerHTML = "<i class='fas fa-volume-off'></i>";
+        playMuteBTN.innerHTML = "Pause music";
     } else {
         count = 0;
         audio.pause();
-        playMuteBTN.innerHTML = "<i class='fas fa-volume-mute'></i>";
+        playMuteBTN.innerHTML = "Play music";
     }
 });
 
