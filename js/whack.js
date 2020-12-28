f1f2a8f8dc7c9814d93b3c5b0ec0fecaeef07aa5
180485 <ismiandhykawaty@icloud.com>
2020-12-28 16:35:11 +0100

    changed whack music background

--- js/whack.js
+++ js/whack.js
@@ -3,9 +3,7 @@ const rats = document.querySelectorAll('.rat');
 const boardScore = document.querySelector('.score');
 const sounds = document.querySelector('#sounds');
 const soundOne = document.querySelector('#soundOne');
-const timerText = document.querySelector('#timer');
-
-var timeleft = 20;
+let timeleft = 20;
 
 
 let moleBefore;
@@ -51,14 +49,8 @@ function play (){
     finish = true ;
     }, 20000);
 
-    /*setInterval(function(){
-        count += 1;
-        timerText.textContent = count;
-        
-        
-        
-    }, 1000);*/
-    var downloadTimer = setInterval(function(){
+    
+    let downloadTimer = setInterval(function(){
         if(timeleft <= 0){
           clearInterval(downloadTimer);
           document.getElementById("timer").innerHTML = "Finished";
